89b2ad7de25bd4cf99f7c1a62f7d536083f081b8
Matt Laux <matthew.laux@gmail.com>
2015-02-15 16:53:41 -0600

    Parse IRC prefix, some scroll bar stuff

--- netutil.h
+++ netutil.h
@@ -1,1 +1,1 @@
-#ifndef NETUTIL_H#define NETUTIL_H#include <MacTypes.h>#include <MacTCP.h>OSErr swritef(StreamPtr stream, const char *fmt, ...);void spoll(StreamPtr stream, void (*process_line)(const char *));#endif
+#ifndef NETUTIL_H#define NETUTIL_H#include <MacTypes.h>#include <MacTCP.h>OSErr swritef(StreamPtr stream, const char *fmt, ...);void spoll(StreamPtr stream, void (*process_line)(char *));#endif
