cbf5f4c8783463692577ae74e7a7369a4b9516e8
Matthew Laux <mlaux@cs.utexas.edu>
2015-02-18 17:29:17 -0600

    Fix line endings and add filter to .gitattributes

--- irc.h
+++ irc.h
@@ -1,1 +1,16 @@
-#ifndef IRC_H#define IRC_H#define PRIVMSG 0#define NOTICE 1#define IRC_LINE_MAX 512#define IRC_PARAM_MAX 15void send_userinfo(StreamPtr stream, const char *nick, const char *ident, const char *realname);void send_pong(StreamPtr stream, const char *token);void send_join(StreamPtr stream, const char *channel);void send_part(StreamPtr stream, const char *channel);void send_message(StreamPtr stream, int type, const char *target, const char *message);#endif
+#ifndef IRC_H
+#define IRC_H
+
+#define PRIVMSG 0
+#define NOTICE 1
+
+#define IRC_LINE_MAX 512
+#define IRC_PARAM_MAX 15
+
+void send_userinfo(StreamPtr stream, const char *nick, const char *ident, const char *realname);
+void send_pong(StreamPtr stream, const char *token);
+void send_join(StreamPtr stream, const char *channel);
+void send_part(StreamPtr stream, const char *channel);
+void send_message(StreamPtr stream, int type, const char *target, const char *message);
+
+#endif
